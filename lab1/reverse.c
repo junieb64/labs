373614da790842202ce5127881ec440673f1f715
Dani Romero Mejia <dani.romeromejia@gmail.com>
2026-05-05 09:20:50 +0000

    PASSED ALL TESTS B)

--- lab1/reverse.c
+++ lab1/reverse.c
@@ -9,7 +9,7 @@
  * elements in the array. It's responsible for reversing the input array. You
  * should be modifying this array in place. Meaning that you shouldn't returning
  * anything from this function.
- *
+ * 
  * 2. swap: will take in two elements from the array and swap them. Returns
  * nothing.
  *
@@ -17,3 +17,25 @@
  * Given an array ["hello", "my", "name", "is"], after calling reverse_arr
  * will result in ["is", "name", "my", "hello"]
  */
+void swap(char** aa, char** bb) {
+    char* a = *aa;
+    char* b = *bb;
+
+    for (int i = 0; *(a + i) != '\0'; i++) {
+    // prints out each character
+        printf("character: %c\n", *(a + i));
+    }
+    *aa = b;
+    *bb = a;
+}
+
+void reverse_arr(char** arr, int num) {
+    printf("reversing\n");
+    char** aa = arr;
+    char** bb = arr + (num-1);
+    while (bb > aa) {
+         swap(aa, bb);
+         aa++;
+         bb--;
+    }
+}
